669c46b9d76971f2cf5f299b64ba37cc5f204bc1
Air Dan <danshandle@gmail.com>
2019-08-10 23:54:48 -0500

    js objects

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+const words = ["touchdown", "rams", "colliseum", "sack", "gridiron","gregtheleg","punt","interception","defense","gurley","tackle"]
+
+let rand = "";
+let word = "";
+let blanks = [];
+let guesses = 0;
+let letters = [];
+let wins = 0;
+let losses = 0;
+let lock = false;
+let alpha = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+
